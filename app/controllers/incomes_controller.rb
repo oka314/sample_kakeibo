a7957cd84b208e6a02d0572dccf471105d4b02a3
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-05 13:39:46 +0900

    ij

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,25 +1,35 @@
 class IncomesController < ApplicationController
   def index
-    @incomes = Incomes.order(created_at: :asc)
+    @incomes = Income.order(created_at: :asc)
   end
 
   def show
+    @income = Income.find(params[:id])
   end
 
   def new
+    @income = Income.new()
   end
 
   def edit
+    @income = Income.find(params[:id])
   end
 
   def create
+    @income = Income.new(@params[:income])
+    if @income.save
+      redirect_to @income, notice: "おしごとを作ったよ！"
+    else
+      render "new"
+    end
   end
-
+  
+  def update
+    @income = Income.find(params[:id])
+    @income.assign
+  end
+  
   def destroy
   end
-
-  def update
-  end
-
   
 end
